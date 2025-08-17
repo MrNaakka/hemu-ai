e3ad02ee07bf63ac13922872e65650695b67e5d2
MrNaakka <herman.virtanen@gmail.com>
2025-08-17 19:56:35 +0300

    fixed bug where the whole screen went to scroll mode.

--- src/app/_components/shortcuts.tsx
+++ src/app/_components/shortcuts.tsx
@@ -24,25 +24,36 @@ export default function ShortCuts({
   };
 
   return (
-    <div className="flex flex-row overflow-x-auto border-b-1 border-teal-900 p-4 whitespace-nowrap">
-      {shortcuts.map((shortcut) => (
-        <Tooltip key={crypto.randomUUID()} disableHoverableContent={true}>
-          <TooltipTrigger asChild>
-            <button
-              className="hover:bg-secondaryBg mr-2 ml-2 flex-shrink-0 items-center justify-center p-3"
-              onClick={() =>
-                handleShortCutClick(shortcut.action, shortcut.fn === "write")
-              }
-              onMouseDown={(e) => e.preventDefault()}
-            >
-              <img alt="error" className="invert filter" src={shortcut.svg} />
-            </button>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>{shortcut.action}</p>
-          </TooltipContent>
-        </Tooltip>
-      ))}
+    <div className="relative w-full min-w-0">
+      <div className="absolute right-0 left-0 overflow-x-auto">
+        <div className="flex flex-row overflow-x-auto border-b-1 border-teal-900 pr-4 pl-4 whitespace-nowrap">
+          {shortcuts.map((shortcut) => (
+            <Tooltip key={crypto.randomUUID()} disableHoverableContent={true}>
+              <TooltipTrigger asChild>
+                <button
+                  className="hover:bg-secondaryBg mr-2 ml-2 flex-shrink-0 items-center justify-center p-3"
+                  onClick={() =>
+                    handleShortCutClick(
+                      shortcut.action,
+                      shortcut.fn === "write",
+                    )
+                  }
+                  onMouseDown={(e) => e.preventDefault()}
+                >
+                  <img
+                    alt="error"
+                    className="invert filter"
+                    src={shortcut.svg}
+                  />
+                </button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{shortcut.action}</p>
+              </TooltipContent>
+            </Tooltip>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
